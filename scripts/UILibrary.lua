61fcc24bbc4233d8f4dc8899f456744f17e90772
Tea-is-a-hottie <96389864+Tea-is-a-hottie@users.noreply.github.com>
2021-12-19 13:47:06 -0500

    UILibrary.lua

--- scripts/UILibrary.lua
+++ scripts/UILibrary.lua
@@ -158,7 +158,7 @@ local library = {
         guiname = "Offbrand Cute-ware™️",
 		title = "Offbrand Cute-ware™️ [Hexagon Copy edition]",
 		logo = "rbxassetid://4350178803", -- "http://www.roblox.com/asset/?id=6597976562",
-        footer = " Exodus Winning $$$",
+        footer = "Offbrand Cute-ware winning",
         modal = true,
         font = Enum.Font.SourceSans,
         textsize = 16,
